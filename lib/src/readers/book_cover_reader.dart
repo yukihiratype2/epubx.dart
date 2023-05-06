d4ce626754919b2177ebd222007a58c6b346d0ab
yukihiratype2 <yukihiratype2@live.com>
2023-05-06 20:52:17 +0800

    fix

--- lib/src/readers/book_cover_reader.dart
+++ lib/src/readers/book_cover_reader.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:typed_data';
 
 import 'package:collection/collection.dart' show IterableExtension;
 import 'package:image/image.dart' as images;
@@ -40,7 +41,8 @@ class BookCoverReader {
     coverImageContentFileRef = bookRef.Content!.Images![coverManifestItem.Href];
     var coverImageContent =
         await coverImageContentFileRef!.readContentAsBytes();
-    var retval = images.decodeImage(coverImageContent);
+    var uint8List = Uint8List.fromList(coverImageContent);
+    var retval = images.decodeImage(uint8List);
     return retval;
   }
 }
